d844cb7883a6c9ac1fecd527f833f43b9940cfc4
Vulpelo <damian.praski@gmail.com>
2018-06-18 12:58:29 +0200

    Property

---
+++ src/main/java/sample/Config.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Config {
+    static boolean[][]shopMap = {
+            {false, false, false, false,  true,  true,  true,  true,  true, false},
+            {false,  true,  true,  true,  true, false, false, false,  true, false},
+            {false,  true, false, false,  true, false, false, false,  true, false},
+            {false,  true, false, false,  true,  true,  true,  true,  true, false},
+            {false,  true, false, false,  true, false, false, false,  true,  true},
+            {false,  true,  true,  true,  true, false, false, false,  true,  true},
+            {false,  true, false, false,  true,  true,  true,  true,  true, false},
+            {false,  true, false, false,  true, false, false, false,  true, false},
+            {false,  true, false, false,  true, false, false, false,  true, false},
+            {false,  true, false, false,  true,  true,  true,  true,  true, false},
+            { true,  true,  true,  true,  true, false, false, false,  true, false},
+    };//false-sciana/pulka
+    static String dataBaseFile = "./database.xls";
+}
